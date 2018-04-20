cc4b5374dd46e3274813e3bd0d12ced80bc757fa
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-04-20 08:39:31 +0100

    port updated

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log(`Server is unop on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 
