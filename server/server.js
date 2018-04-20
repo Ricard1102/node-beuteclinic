81471678a1c0df68c080616cb98cc440d5d1fd9e
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-04-20 19:22:13 +0100

    nodemailer dev 3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const bodyParse = require('body-parser');
+//const bodyParse = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
 
@@ -15,17 +15,17 @@ app.set('view engine', 'handlebars');*/
 
 //BODY PARSER MIDDLEWARE
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 //static folder
 /*app.use('/public', express.static(path.join(__dirname, 'public')));*/
 
 //ROUTES
 
-//app.use(express.static(publicPath));
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
