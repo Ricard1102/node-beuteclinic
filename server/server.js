17b6fca5d1744986c38690496922e6d41c508982
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-04-20 19:19:07 +0100

    nodemailer dev 2

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 //ROUTES
 
-app.use(express.static(publicPath));
+//app.use(express.static(publicPath));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
